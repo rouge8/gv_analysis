157dd57018d568f31d3afd65490de3d2b1a6f0d0
Andy Freeland <andy@andyfreeland.net>
2012-04-08 23:45:37 -0500

    support searching by YEAR and added all sorts of new stats

--- timecard.py
+++ timecard.py
@@ -2,8 +2,13 @@ from BeautifulSoup import BeautifulSoup
 import os
 import parse
 import spark
-from collections import Counter
+from collections import Counter, namedtuple
 from pytz import timezone
+from datetime import datetime, date
+import sys
+import codecs
+
+YEAR = None
 
 def get_names(files):
     names = []
@@ -31,13 +36,34 @@ def analyze(name, files):
                 time = parse.parse('{:ti}', t.get('title')).fixed[0]
                 if time:
                     time = time.astimezone(central)
-                times.append(time)
+
+                if YEAR:
+                    if time.year == YEAR: times.append(time)
+                else:
+                    times.append(time)
 
     if len(times) == 0:
         return
 
+    days = sorted({t.date() for t in times})
+    total = len(times)
+
+    span = (days[-1] - days[0]).days
+    span = span if span >= 1 else 1
+    texts_per_day = (total*1.0)/span
+
+    if not YEAR or date(YEAR, 12, 31) > datetime.now().date():
+        avg_span = (datetime.now().date() - days[0]).days
+    elif YEAR and days[0] > date(YEAR, 1, 1):
+        avg_span = (date(YEAR,12,31) - days[0]).days
+    elif YEAR:
+        avg_span = (date(YEAR,12, 31) - date(YEAR,1,1)).days
+
+    avg_span = avg_span if avg_span >= 1 else 1
+    avg = (total*1.0) / avg_span
+
     if name:
-        print 'Messaged %s %d times.' %(name, len(times))
+        print 'Messaged %s %d times, %f/day.' %(name, total, avg)
     else:
         print 'Messaged everyone %d times.' %(len(times))
 
@@ -47,13 +73,14 @@ def analyze(name, files):
     #for i in range(24):
         #print "%d: %d texts." %(i, hour_count[i])
 
-    graph_data = [hour_count.get(i, 0) for i in range(24)]
-    spark.spark_print(graph_data)
+    hour_data = [hour_count.get(i, 0) for i in range(24)]
+    spark.spark_print(hour_data)
     print
     print
 
-import sys
-import codecs
+    Count = namedtuple('Count', ['name', 'count', 'avgday', 'avg'])
+    return Count(name, total, texts_per_day, avg)
+
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 def main():
@@ -61,9 +88,41 @@ def main():
     os.chdir('conversations')
     files = [f for f in files if f.endswith('html')]
     names = get_names(files)
+
+    message_counts = []
+
     for name in names:
-        analyze(name, files)
+        result = analyze(name, files)
+        if result:
+            message_counts.append(result)
     analyze(None, files)
+
+    # totals
+    print 'TOP TEN TEXTERS:'
+    top = [(i.name, i.count) for i in sorted(message_counts, key=lambda k: k.count, reverse=True) if i.count > 0]
+    #for i in range(10):
+    for i in range(len(top)):
+        print '%d.' %(i+1), top[i][0], top[i][1]
+
+    print
+
+    # averages
+    print 'TOP TEN TEXTERS ON DAILY AVERAGES:'
+    avgs = [(i.name, round(i.avgday,2)) for i in sorted(message_counts, key=lambda k: k.avgday, reverse=True) if i.avgday > 0 and i.avgday != i.count]
+    for i in range(len(avgs)):
+    #for i in range(10):
+        print '%d.' %(i+1), avgs[i][0], avgs[i][1]
+
+    print
+
+    # actual averages
+    print 'TOP TEXTERS FOR ACTUAL AVERAGES SINCE FIRST TEXT:'
+    actual_avgs =  [(i.name, round(i.avg,2)) for i in sorted(message_counts, key=lambda k: k.avg, reverse=True) if i.count > 0 and i.count != i.avg ]#and i.avg > 1]
+
+    for i, a in enumerate(actual_avgs):
+        print '%d.' %(i+1), a[0], a[1]
+
+
 
 
 if __name__ == '__main__':
