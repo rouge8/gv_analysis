5016083c1114cedd0252d9244fbc719b446748c9
Andy Freeland <andy@andyfreeland.net>
2012-06-03 21:12:24 -0500

    Update master

--- settings_example.py
+++ settings_example.py
@@ -3,4 +3,4 @@ DATABASE ='gv.db' # path to sqlite database
 CONVERSATIONS_DIR = 'conversations' # path to directory of google voice transcripts
 
 OWNER_NAME = 'Your Name' # name of google voice user
-OWNER_PHONE = '12225551234' # 11-digit phone number of google voice user
+OWNER_PHONES = ('12225551234', '12345678901') # 11-digit phone number of google voice user
